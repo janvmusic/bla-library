b88decce630e8e3d4782598f172e8acf405bb0ef
Jorge A Nunez <1697540+janvmusic@users.noreply.github.com>
2026-03-18 13:03:22 -0600

    feat(JNU-16): add Book model validations, available_copies method, and specs (#6)

--- backend/app/models/book.rb
+++ backend/app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ApplicationRecord
+  has_many :book_reservations
+
+  validates :title, :author, :isbn, :total_copies, presence: true
+  validates :isbn, uniqueness: true
+  validates :total_copies, numericality: { greater_than_or_equal_to: 1 }
+
+  def available_copies
+    total_copies - book_reservations.where(returned_at: nil).count
+  end
 end
